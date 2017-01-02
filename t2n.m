e236b082fa91b98b1c45fb196c7fc6314e5aa555
Marcel <marcel.beining@gmail.com>
2017-01-02 17:58:27 +0100

    V039: outsorcing t2n checkinput

--- t2n.m
+++ t2n.m
@@ -280,71 +280,8 @@ end
 
 
 % check input
+[ thesetrees,usestreesof ] = t2n_checkinput(neuron,tree);
 
-thesetrees = cell(numel(neuron),1);
-usestreesof = zeros(numel(neuron),1);
-flag = false;
-bool = cellfun(@(y) isfield(y,'tree'),neuron);
-if all(cellfun(@(y) ischar(y.tree),neuron(bool))) % no trees defined (only due to t2n_as function). make bool = 0
-    bool(:) = 0;
-end
-nempty = cellfun(@isempty,neuron);
-if any(nempty)
-    errordlg(sprintf('The defined simulation #%d is empty, please check\n',find(nempty)))
-    out = t2n_error(out,outoptions);
-    return
-end
-switch sum(bool)
-    case 1   % use that treeids defined in that one simulation
-        if isnumeric(neuron{bool}.tree)
-            thesetrees = repmat({unique(neuron{bool}.tree)},numel(neuron),1);
-            usestreesof = repmat(find(bool),numel(neuron),1);
-        else
-            x = getref(1,neuron,'tree');
-            if isempty(x) % means it refers to itself (maybe due to usage of t2n_as)..use normal trees..
-                thesetrees = repmat({1:numel(tree)},numel(neuron),1);
-                usestreesof = ones(numel(neuron),1);
-                for n = 1:numel(neuron)
-                    neuron{n}.tree = thesetrees{n};
-                end
-            else
-                n = find(bool);
-                flag = true;
-            end
-        end
-    case 0      % if no trees are given, use trees that are given to t2n in their order...
-        thesetrees = repmat({1:numel(tree)},numel(neuron),1);
-        usestreesof = ones(numel(neuron),1);
-        for n = 1:numel(neuron)
-            neuron{n}.tree = thesetrees{n};
-        end
-    case numel(neuron)
-        for n = 1:numel(neuron)
-            x = getref(n,neuron,'tree');
-            if ~isnan(x)
-                thesetrees{n} = unique(neuron{x}.tree);
-                usestreesof(n) = x;
-            elseif isempty(x)
-                thesetrees{n} = 1:numel(tree);%repmat({1:numel(tree)},numel(neuron),1);
-                usestreesof(n) = 1;%ones(numel(neuron),1);
-%                 for n = 1:numel(neuron)
-                    neuron{n}.tree = thesetrees{n};
-%                 end
-            else
-                flag = true;
-                break
-            end
-        end
-    otherwise  % if more than one are given, t2n cannot know which trees you want
-        n = find(bool);
-        flag = true;
-end
-if flag
-    origminterf = [];
-    errordlg(sprintf('Error in neuron{%d}.tree, please check\n',n))
-    out = t2n_error(out,outoptions);
-    return
-end
 %
 for t = 1:numel(tree)
     if isfield(tree{t},'artificial') && ~isfield(tree{t},'NID')
@@ -818,9 +755,6 @@ for n = 1:numel(neuron)
                                                 else
                                                     str = sprintf('%s%s = %g\n',str,mechpar{p},neuron{n}.mech{t}.(regs{r}).(mechs{m}).(mechpar{p}));   %neuron: define values
                                                 end
-                                                
-                                                
-                                                
                                                 
                                             else
                                                 if numel(neuron{n}.mech{t}.(regs{r}).(mechs{m}).(mechpar{p})) == 1
