da822cb67ff958713f8ab4618e9dbce784f17c64
MBeining <marcel.beining@brain.mpg.de>
2017-01-24 13:06:35 +0100

    V044: new utility files

---
+++ poissonSpikeGen.m
@@ -0,0 +1,12 @@
+function [spikeMat, tVec] = poissonSpikeGen(freq, params, nTrials)
+% dt, tstop and tvec in ms, freq in Hz
+
+if nargin < 3
+    nTrials = 1;
+end
+
+%    params.dt = params.dt/1000; %ms to s 
+
+nBins = floor(params.tstop/params.dt);
+spikeMat = rand(nTrials, nBins) < freq*params.dt/1000;
+tVec = 0:params.dt:params.tstop-params.dt;
